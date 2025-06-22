2354a0451c6a15edfbba0987d4c6caddd0e8ffd2
Anjlee Duseja <anjleeduseja13@gmail.com>
2025-06-22 14:50:54 +0500

    updated counter app

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -27,19 +27,23 @@ function Counter() {
 
   return (
 
-    <div className='bg-gray-100 border border-gray-200 h-70  mx-10 flex flex-col items-center  shadow-lg shadow-gray/40 rounded-lg'>
+    <div className='bg-gray-100 border border-gray-200 min-h-[300px]  mx-10 flex flex-col items-center  shadow-lg shadow-gray/40 rounded-lg min-w-[200px]'>
         <h1 className=' italic text-2xl font-bold bg-black text-white text-center py-3 mb-10 w-full rounded shadow-lg shadow-black/20'>Counter App</h1>
-    <div className="flex items-center justify-center gap-10 mt-5">
-       <button className="hover:border hover:bg-white hover:border-black hover:text-black bg-black text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300"
+    <div className="flex flex-wrap items-center justify-center gap-10 mt-5">
+      <div>
+       <button className="hover:border hover:bg-white hover:border-black hover:text-black bg-black text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300 mx-2"
        onClick={()=>handleCounter("decrease", 5)} > Decrease by 5 </button>
-       <button className="border border-black-200 text-black-500 hover:border-transparent hover:bg-black hover:text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300"
+       <button className="border border-black-200 text-black-500 hover:border-transparent hover:bg-black hover:text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300 mx-2"
       onClick={()=>handleCounter("decrease", 1)}
       >Decrease</button>
-        <span className="mx-2 font-bold text-4xl rounded-full bg-black text-white p-6">{counter}</span>
-      <button className="border border-black-200 text-black-500 hover:border-transparent hover:bg-black hover:text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300"
+      </div>
+        <div className="mx-2 font-bold text-4xl rounded-full bg-black text-white p-6">{counter}</div>
+     <div>
+      <button className="border border-black-200 text-black-500 hover:border-transparent hover:bg-black hover:text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300 mx-2"
        onClick={()=>handleCounter("increase", 1)} > Increase </button>
-       <button className="hover:border hover:bg-white hover:border-black hover:text-black bg-black text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300"
+       <button className="hover:border hover:bg-white hover:border-black hover:text-black bg-black text-white active:bg-black-700 px-4 py-1 rounded-sm transition-colors duration-300 mx-2"
        onClick={()=>handleCounter("increase", 5)} > Increase by 5 </button>
+       </div>
     </div>
     </div>
   )
